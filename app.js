6bd49ad7f729be541eb98c6eacdb118e8c9c4865
Arjun Das <arjundasback515@gmail.com>
2022-08-31 20:23:47 +0600

    dynamic value show the website

--- app.js
+++ app.js
@@ -16,9 +16,9 @@ const displayData = (phones)=>{
         createDiv.innerHTML=`
         <div class="col">
                     <div class="card">
-                        <img src="" class="card-img-top" alt="...">
+                        <img src="${phone.image}" class="card-img-top" alt="...">
                         <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
+                            <h5 class="card-title">${phone.phone_name}</h5>
                             <p class="card-text">This is a longer card with supporting text below as a natural lead-in
                                 to additional content. This content is a little bit longer.</p>
                         </div>
